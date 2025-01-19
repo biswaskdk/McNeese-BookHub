71eeae81a4f6fe5cb5a2d864015ed3da0aaa6b9c
blazingpyro <biswaskhadka10@gmail.com>
2025-01-19 08:52:32 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+        <link rel="stylesheet" href="main.css">
+    </head>
+    <body>
+        <form method="post" action="index.php">
+            <h1>Login</h1>
+            <div>
+                <input type="text" placeholder="Username" name="username">
+            </div>
+            <div>
+                <input type="password" placeholder="Password" name="password">
+            </div>
+            <input type="submit" value="Login" name="login_Btn">
+            <div>
+                Don't have an account ?
+            </div>
+        </form>
+    </body>
+</html>
+<?php
+$conn = mysqli_connect("localhost", "root", "");
+if(isset($_POST['login_Btn'])){
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+    $sql= "SELECT * FROM websitelogin.logindetails WHERE username = '$username'";
+    $result = mysqli_query($conn,$sql);
+    while($row = mysqli_fetch_assoc($result)){
+        $resultPassword = $row['password'];
+        if($password == $resultPassword){
+            header('Location:bookHub.html');
+        }else{
+            echo "<script>
+            alert('Login unsuccessful');
+            </script>";
+        }
+    }
+}
+?>
